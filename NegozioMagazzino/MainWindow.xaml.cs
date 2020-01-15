d7b93a54e95c57659104f43ffdcc94c773ecb3bd
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-01-15 13:11:59 +0100

    Add project files.

---
+++ NegozioMagazzino/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NegozioMagazzino
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string file = "file.txt";
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnVedi_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(file))
+                try
+                {
+                    using (StreamReader rdfile = new StreamReader(file))
+                    {
+                        string sline;
+                        while ((sline = rdfile.ReadLine()) != null)
+                        {
+                            LstOggetti.Items.Add(sline.ToString());
+                        }
+                    }
+                }
+                catch
+                {
+                    MessageBox.Show("Il file non esiste");
+                }
+        }      
+
+        private void BtnAggiungi_Click(object sender, RoutedEventArgs e)
+        {
+            using (StreamWriter swoggetti = new StreamWriter(file, true))
+            {
+                string prodotto = TxtProdotto.Text;
+                string prezzo = TxtPrezzo.Text;
+                swoggetti.WriteLine($"{prodotto}  {prezzo} €");
+            }
+        }
+    }        
+}   
+
+    
+
